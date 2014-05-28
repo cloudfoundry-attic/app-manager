9a144479e48170352a510c1803699c6fefe9eb72
Onsi Fakhouri <onsi@pivotallabs.com>
2014-05-28 11:43:34 -0700

    Add set diffing delta force (squad)

---
+++ delta_force/delta_force.go
@@ -0,0 +1,63 @@
+package delta_force
+
+type ActualInstance struct {
+	Index int
+	Guid  string
+}
+
+type Result struct {
+	IndicesToStart       []int
+	GuidsToStop          []string
+	IndicesToStopOneGuid []int
+}
+
+type ActualInstances []ActualInstance
+
+func (a ActualInstances) hasIndex(index int) bool {
+	for _, actual := range a {
+		if actual.Index == index {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (a ActualInstances) numAtIndex(index int) int {
+	count := 0
+	for _, actual := range a {
+		if actual.Index == index {
+			count++
+		}
+	}
+
+	return count
+}
+
+func Reconcile(numDesired int, actuals ActualInstances) Result {
+	result := Result{}
+
+	for i := 0; i < numDesired; i++ {
+		if !actuals.hasIndex(i) {
+			result.IndicesToStart = append(result.IndicesToStart, i)
+		}
+	}
+
+	if len(result.IndicesToStart) > 0 {
+		return result
+	}
+
+	for _, actual := range actuals {
+		if actual.Index >= numDesired {
+			result.GuidsToStop = append(result.GuidsToStop, actual.Guid)
+		}
+	}
+
+	for i := 0; i < numDesired; i++ {
+		if actuals.numAtIndex(i) > 1 {
+			result.IndicesToStopOneGuid = append(result.IndicesToStopOneGuid, i)
+		}
+	}
+
+	return result
+}
